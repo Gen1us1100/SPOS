a12559931390006e94ab58cc6b68acdf44707f51
Sameer Godse <coder.sam.1100@gmail.com>
2023-11-14 00:42:00 +0530

    fixed priority code

--- src/college_ass/Priority_scheduling.java
+++ src/college_ass/Priority_scheduling.java
@@ -5,105 +5,94 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Priority_scheduling {
+	class process {
+		public int pid, AT, BT, CT, TAT, WT, Pri;
+		int RT = BT;
+
+		public process() {
+			// TODO Auto-generated constructor stub
+			this.pid = this.AT = this.BT = this.CT = this.TAT = this.WT = this.Pri = 0;
+		}
+	}
+
+	public static void sortProcesses(process[] myProcesses) {
+		process temp;
+		// sorting according to arrival times
+		for (int i = 0; i < myProcesses.length; i++) {
+			for (int j = 0; j < myProcesses.length - (i + 1); j++) {
+				if (myProcesses[j].AT > myProcesses[j + 1].AT && myProcesses[j].Pri > myProcesses[j + 1].Pri) {
+					temp = myProcesses[j];
+					myProcesses[j] = myProcesses[j + 1];
+					myProcesses[j + 1] = temp;
+//					temp = myProcesses[j].pid;
+//					myProcesses[j].pid = myProcesses[j + 1].pid;
+//					myProcesses[j + 1].pid = temp;
+//					temp = myProcesses[j].AT;
+//					myProcesses[j].AT = myProcesses[j + 1].AT;
+//					myProcesses[j + 1].AT = temp;
+//					temp = myProcesses[j].BT;
+//					myProcesses[j].BT = myProcesses[j + 1].BT;
+//					myProcesses[j + 1].BT = temp;
+//					temp = myProcesses[j].RT;
+//					myProcesses[j].RT = myProcesses[j + 1].RT;
+//					myProcesses[j + 1].RT = temp;
+//					temp = myProcesses[j].Pri;
+//					myProcesses[j].Pri = myProcesses[j + 1].Pri;
+//					myProcesses[j + 1].Pri = temp;
+				}
+			}
+		}
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int n = 0;
-
-		int TotalTime = 0; // Total time for all processes
-		int currentTime = 0;// keep track of current time
-
 		Scanner myScanner = new Scanner(System.in);
 		System.out.println("Enter no. of processes: ");
 		n = myScanner.nextInt();
-		
-		int[] pid = new int[n];// Process ID
-		int[] AT = new int[n];// Arrival Time
-		int[] BT = new int[n];// Burst Time
-		int[] pri = new int[n];
-		int[] CT = new int[n];// Completion Time
-		int[] TAT = new int[n];// Turn Around Time = completion time - Arrival time
-		int[] WT = new int[n];// Waiting Time = TAT - Burst Time
-		int[] RT = new int[n];// Remaining Time -- updated BT
-
-		Queue<Integer> readyQueue = new LinkedList<Integer>();
-
-		// take input for arrival time and burst time for each process
+		Priority_scheduling myInstance = new Priority_scheduling(); 
+		process[] processes = new process[n];
 		for (int i = 0; i < n; i++) {
+			processes[i] = myInstance.new process();
 			System.out.println("Enter Arrival time for process P" + (i + 1));
-			AT[i] = myScanner.nextInt();
-			pid[i]= i+1; 
+			processes[i].AT = myScanner.nextInt();
+			processes[i].pid = i + 1;
 			System.out.println("Enter Burst time for process P" + (i + 1));
-			BT[i] = myScanner.nextInt();
+			processes[i].BT = myScanner.nextInt();
 			System.out.println("Enter Priority for process P" + (i + 1));
-			pri[i] = myScanner.nextInt();
-			TotalTime += BT[i];
-			RT[i] = BT[i];
+			processes[i].Pri = myScanner.nextInt();
+//			TotalTime += processes[i].BT;
 		}
 
-		int temp;
-		// sorting according to arrival times
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n - (i + 1); j++) {
-				if (pri[j] > pri[j + 1]) {
-					temp = pid[j];
-					pid[j] = pid[j + 1];
-					pid[j + 1] = temp;
-					temp = AT[j];
-					AT[j] = AT[j + 1];
-					AT[j + 1] = temp;
-					temp = BT[j];
-					BT[j] = BT[j + 1];
-					BT[j + 1] = temp;
-					temp = RT[j];
-					RT[j] = RT[j + 1];
-					RT[j + 1] = temp;
-					temp = pri[j];
-					pri[j] = pri[j + 1];
-					pri[j + 1] = temp;
-				}
+		sortProcesses(processes);
+
+		for (int i = 0; i < processes.length; i++) {
+			if (i == 0) {
+				processes[i].CT = processes[i].BT;
+			} else {
+				processes[i].CT = processes[i - 1].CT + processes[i].BT;
 			}
 		}
-//		for (int i = 0; i < pri.length; i++) {
-//			System.out.println(AT[i] + "\t" + BT[i] + "\t" + RT[i] + "\t" + pri[i]);
-//		}
-		int currentProcess = -1;
-		int completed = 0;
-		while (currentTime < TotalTime) {
-			for (int i = 0; i < n; i++) {
-				if (AT[i] <= currentTime && RT[i] != 0 && (!readyQueue.contains(i))) {
-//					if (completed != (n - 1)) {
-//						if (i != currentProcess && (!readyQueue.contains(i)))
-					readyQueue.add(i);
-//					}
-				}
-			}
-			System.out.println("\nCurrentTime: " + currentTime);
-			System.out.println("QUEUE: " + readyQueue);
-			System.out.println("currentProcess: " + (readyQueue.peek()-1));
-			currentProcess = readyQueue.poll();
-			RT[currentProcess] = 0;
-			currentTime += BT[currentProcess];
-			CT[currentProcess] = currentTime;
-		}
+
 		float totalTAT = 0;
 		float totalWT = 0;
 		for (int i = 0; i < n; i++) {
-			TAT[i] = CT[i] - AT[i];
-			WT[i] = TAT[i] - BT[i];
-			totalTAT += TAT[i];
-			totalWT += WT[i];
+			processes[i].TAT = processes[i].CT - processes[i].AT;
+			processes[i].WT = processes[i].TAT - processes[i].BT;
+			totalTAT += processes[i].TAT;
+			totalWT += processes[i].WT;
 		}
 
 		System.out.println("\n\t\t----Priority scheduling----\t\t");
 		System.out.println("ProcessID\tArrivalT\tBurstT\tCompletionT\tTurnAroundT\tWaitingT\tPriority");
 		for (int i = 0; i < n; i++) {
-			System.out.println("P" + pid[i] + "\t\t" + AT[i] + "\t\t" + BT[i] + "\t\t" + CT[i] + "\t\t" + TAT[i]
-					+ "\t\t" + WT[i] + "\t\t" + pri[i] + "\n");
+			System.out.println("P" + processes[i].pid + "\t\t" + processes[i].AT + "\t\t" + processes[i].BT + "\t\t"
+					+ processes[i].CT + "\t\t" + processes[i].TAT + "\t\t" + processes[i].WT + "\t\t" + processes[i].Pri
+					+ "\n");
 		}
 		System.out.println("Average Turn Around Time: " + (totalTAT / n));
 		System.out.println("Average Waiting Time: " + (totalWT / n));
+
 		myScanner.close();
 	}
-
 }
